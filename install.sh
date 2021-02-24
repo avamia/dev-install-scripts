e28ac66c7fd5794cd00c9a09c0319c1d4f9c6234
Ryan Mammina <ryan@avamia.com>
2021-02-24 01:55:17 +0000

    update install script

--- install.sh
+++ install.sh
@@ -16,4 +16,23 @@ sudo systemctl restart nfs-kernel-server
 
 ## Install RVM
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+## Install Gem Package
+gem install bundler
+gem install rails
+
+## Install Nodejs
+sudo apt install nodejs npm -y
+
+## Install Redis
+sudo apt install redis-server -y
+
+## Install Postgres Client
+sudo apt update
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt install postgresql-client-13
+
+
 
