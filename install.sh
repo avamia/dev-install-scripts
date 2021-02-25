40d3e1d7d0215a0b28f4673f20ae54432fd58d08
Ryan Mammina <ryan@avamia.com>
2021-02-25 03:15:42 +0000

    add yarn support and postgres server

--- install.sh
+++ install.sh
@@ -23,16 +23,13 @@ gem install bundler
 gem install rails
 
 ## Install Nodejs
-sudo apt install nodejs npm -y
+sudo apt install nodejs npm cmdtest -y
 
 ## Install Redis
 sudo apt install redis-server -y
 
-## Install Postgres Client
+## Install Postgres
 sudo apt update
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee  /etc/apt/sources.list.d/pgdg.list
-sudo apt install postgresql-client-13
-
-
-
+sudo apt install libpq-dev postgresql-13 postgresql-client-13 -y
